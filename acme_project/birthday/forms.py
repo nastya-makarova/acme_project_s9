0a699df90b9d88b7c5ec455cfa9b67d365d213b4
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-07-19 21:45:50 +0400

    Added functionality for validation using validators and clean methods.

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 
+# Импортируем класс ошибки валидации.
+from django.core.exceptions import ValidationError
+
 from .models import Birthday
+
+
+# Множество с именами участников Ливерпульской четвёрки.
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
 
 class BirthdayForm(forms.ModelForm):
@@ -11,3 +18,22 @@ class BirthdayForm(forms.ModelForm):
         widgets = {
             'birthday': forms.DateInput(attrs={'type': 'date'})
         }
+
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам 
+        # и возвращаем только первое имя.
+        return first_name.split()[0]
+
+    def clean(self):
+        # Вызов родительского метода clean.
+        super().clean()
+        # Получаем имя и фамилию из очищенных полей формы.
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
