7a4f35cde17d3ca8d47edef64e9686173953e7ff
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-07-19 18:17:25 +0400

    Added feature to delete data.

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.birthday, name='create'),
     path('list/', views.birthday_list, name='list'),
     path('<pk>/edit', views.birthday, name='edit'),
+    path('<pk>/delete', views.birthday_delete, name='delete'),
 ]
