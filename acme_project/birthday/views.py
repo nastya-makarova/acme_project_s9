ab48c95bd5dd5b520be14875064fcdc1980418ea
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-07-20 22:35:08 +0400

    Added pagintor.

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
+# Импортируем класс пагинатора.
+from django.core.paginator import Paginator
 
 from .forms import BirthdayForm
 from .models import Birthday
@@ -45,9 +47,19 @@ def birthday(request, pk=None):
 
 def birthday_list(request):
     # Получаем все объекты модели Birthday из БД.
-    birthdays = Birthday.objects.all()
-    # Передаём их в контекст шаблона.
-    context = {'birthdays': birthdays}
+    birthdays = Birthday.objects.order_by('id')
+    # Создаём объект пагинатора с количеством 10 записей на страницу.
+    paginator = Paginator(birthdays, 3)
+
+    # Получаем из запроса значение параметра page.
+    page_number = request.GET.get('page')
+    # Получаем запрошенную страницу пагинатора.
+    # Если параметра page нет в запросе или его значение не приводится к числу,
+    # вернётся первая страница.
+    page_obj = paginator.get_page(page_number)
+    # Вместо полного списка объектов передаём в контекст
+    # объект страницы пагинатора
+    context = {'page_obj': page_obj}
     return render(request, 'birthday/birthday_list.html', context)
 
 
