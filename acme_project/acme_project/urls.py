bd7504d49ab872fe0bbeb6d2d0b101a418b693a4
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-07-23 00:16:47 +0400

    Added functionality for authentification and logging out.

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('auth/', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
