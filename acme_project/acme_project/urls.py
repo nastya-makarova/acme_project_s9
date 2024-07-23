f4589f597d32f651484a46715cbd70ab79dbbbe2
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-07-24 00:01:19 +0400

    Added registation for new users.

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -3,10 +3,22 @@ from django.conf import settings
 # Импортируем функцию, позволяющую серверу разработки отдавать файлы.
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import include, reverse_lazy, path
+from django.views.generic.edit import CreateView
+
 
 urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
